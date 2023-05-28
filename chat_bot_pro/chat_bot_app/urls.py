db538ac2e24057696d530c8e2011c256909661ca
Abhijith Babu <abhijith33c@gmail.com>
2023-05-28 23:10:25 +0530

    first commit

--- chat_bot_pro/chat_bot_app/urls.py
+++ chat_bot_pro/chat_bot_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('chat', chat, name='chat')
+    path('home', home, name='home'),
+    path('chat', chat, name='chat'),
+    path('about', about, name='about'),
+    path('chatAPI', chatAPI, name='chatAPI'),
+    
 
 ]
--- chat_bot_pro/chat_bot_app/views.py
+++ chat_bot_pro/chat_bot_app/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render,HttpResponse
-
+from django.http import JsonResponse
 # Create your views here.
+import os
+import openai
 
-def chat(request):
+openai.api_key = "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"
+
+
+
+def home(request):
     return render(request,'index.html')
 
+def about(request):
+    return render(request,'about.html')
+
+def chat(request):
+    return render(request,'chat.html')
+
 def chatAPI(request):
-    return HttpResponse("Welcome to chat bot")
+    if request.method=="POST":
+        print(request.prompt)
+        response={"this":"that"}
+    # response = openai.Completion.create(
+
+    #     model="text-davinci-003",
+    #     prompt="",
+    #     temperature=1,
+    #     max_tokens=256,
+    #     top_p=1,
+    #     frequency_penalty=0,
+    #     presence_penalty=0
+    # )
+    
+    return JsonResponse(response)
